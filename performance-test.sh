662b03fd80180f7fc650b7ce2e399d05ba9ade73
Ilham Muhammad Arif <osis.ilham99@gmail.com>
2026-06-21 02:26:53 +0700

    Use docker to run hey

--- performance-test.sh
+++ performance-test.sh
@@ -7,9 +7,5 @@ NODE_IP=$(kubectl --context=kind-staging --insecure-skip-tls-verify=true get nod
 NODE_PORT=$(kubectl --context=kind-staging --insecure-skip-tls-verify=true get svc hello -o=jsonpath='{.spec.ports[0].nodePort}')
 ENDPOINT="${NODE_IP}:${NODE_PORT}"
 
-echo "Downloading hey..."
-curl -fsSL -o hey https://hey-release.s3.us-east-2.amazonaws.com/hey_linux_amd64
-chmod +x hey
-
-echo "Running load test with hey..."
-./hey -n 1000 -c 50 http://${ENDPOINT}/hello
+echo "Running load test with hey via Docker..."
+docker run --rm --network kind williamyeh/hey -n 1000 -c 50 http://${ENDPOINT}/hello
